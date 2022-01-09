c7f4cb1006762e4fcc31d4c4b3f265e9da0a517f
Liem Mazal <liemmazal5@gmail.com>
2022-01-09 17:27:07 +0200

    Changing the edit fragment

--- app/src/main/java/com/example/kinderview/fragment_edit_post.java
+++ app/src/main/java/com/example/kinderview/fragment_edit_post.java
@@ -1,8 +1,11 @@
 package com.example.kinderview;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
@@ -14,6 +17,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.kinderview.model.Post;
+import com.example.kinderview.viewModel.CreatePostViewModel;
 import com.example.kinderview.viewModel.EditViewModel;
 
 
@@ -25,6 +29,13 @@ public class fragment_edit_post extends Fragment {
     ProgressBar progressBar;
     EditViewModel editViewModel;
     Button editButton, cancelButton;
+    String id, status,username,date_post,likes;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        editViewModel = new ViewModelProvider(this).get(EditViewModel.class);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,6 +51,16 @@ public class fragment_edit_post extends Fragment {
 
         progressBar = view.findViewById(R.id.fragment_edit_progressbar);
         progressBar.setVisibility(View.GONE);
+
+         id = fragment_edit_postArgs.fromBundle(getArguments()).getId();
+         status = fragment_edit_postArgs.fromBundle(getArguments()).getStatus();
+         username = fragment_edit_postArgs.fromBundle(getArguments()).getUsername();
+         date_post = fragment_edit_postArgs.fromBundle(getArguments()).getDate();
+         likes = fragment_edit_postArgs.fromBundle(getArguments()).getLikes();
+
+        edit_username.setText(username);
+        edit_date.setText(date_post);
+        edit_status.setText(status);
 
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,18 +79,13 @@ public class fragment_edit_post extends Fragment {
         editButton.setEnabled(false);
         cancelButton.setEnabled(false);
 
-        String id = fragment_edit_postArgs.fromBundle(getArguments()).getId();
-        String status = fragment_edit_postArgs.fromBundle(getArguments()).getStatus();
-        String username = fragment_edit_postArgs.fromBundle(getArguments()).getUsername();
-        String date_post = fragment_edit_postArgs.fromBundle(getArguments()).getDate();
-        String likes = fragment_edit_postArgs.fromBundle(getArguments()).getLikes();
+        String id1 =id;
+        String status1 = edit_status.getText().toString();
+        String username1 = username;
+        String date_post1 = edit_date.getText().toString();
+        String likes1 = likes;
 
-        edit_username.setText(username);
-        edit_date.setText(date_post);
-        edit_status.setText(status);
-
-
-        Post post = new Post(id, status, username, date_post, likes);
+        Post post = new Post(id1, status1, username1, date_post1, likes1);
 
         editViewModel.editPost(post,()->
         {
