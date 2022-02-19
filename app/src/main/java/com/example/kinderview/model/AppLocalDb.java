b603a0003185166b8f40703736f6f9fa0c9a601f
amit ben basat <amitbasat124@gmail.com>
2022-02-19 17:57:50 +0200

    the updated project -final

--- app/src/main/java/com/example/kinderview/model/AppLocalDb.java
+++ app/src/main/java/com/example/kinderview/model/AppLocalDb.java
@@ -4,7 +4,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Post.class,Profile.class}, version = 59)
+@Database(entities = {Post.class,Profile.class}, version = 60)
 abstract class AppLocalDbRepository extends RoomDatabase {
     public abstract PostDAO PostDao();
     public abstract ProfileDAO profileDAO();
